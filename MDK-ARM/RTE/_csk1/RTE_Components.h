74751768817fb07bd44ecd0b7d9bec010f0d2bdc
csk <chenshukang@sjtu.edu.cn>
2025-10-01 14:38:18 +0800

    initial

---
+++ MDK-ARM/RTE/_csk1/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'csk1' 
+ * Target:  'csk1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
